e50194ae8788447ea2a51e3e35c69dca8e7ec3a9
AnkitSG07 <ankitsinghgaria@gmail.com>
2025-04-22 16:49:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,8 @@ from datetime import datetime
 from cryptography.fernet import Fernet
 import google.generativeai as genai
 from google.generativeai import GenerativeModel
-from SmartApi.smartConnect import SmartConnect
+import smartapi
+from smartapi.smartConnect import SmartConnect
 
 
 fernet = Fernet(os.environ["FERNET_KEY"])
